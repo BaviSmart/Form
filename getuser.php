1dd29423df65f28c90fd5a0049cd7a84ae22f070
BaviSmart <116780725+BaviSmart@users.noreply.github.com>
2023-02-01 21:52:15 +0530

    Delete getuser.php

--- getuser.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include 'db.php';
-
-$sql = "SELECT * FROM `tbl_student_bavi` where studid='$_POST[id]'";
-$result = $db->query($sql);
-$row = $result->fetch_assoc();
-
-echo json_encode($row);
-?>
